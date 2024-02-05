7fc7fd8168c71bccab3e6922b7c36fb0cb34135e
Wang-Hsing Chen <chenwils@usc.edu>
2024-02-05 08:39:25 -0800

    2024-02-05 Main page add email

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,11 +11,14 @@ export default function Home() {
                     <p className={styles.title}>WILSON CHEN</p>
                     <p className={styles.subTitle}>UNIVERSITY OF SOUTHERN CALIFORNIA</p>
                     <p className={styles.subTitle}>B.S COMPUTER SCIENCE</p>
+                    <br/>
+                    <br/>
+                    <p className={styles.subTitle}>chenwils@usc.edu</p>
                 </div>
                 <img className={styles.profilePic} src='ProfilePic.jpg' alt='profile pic'/>
                 <div className={styles.selectionBar}>
                     <span className={styles.mobileTitle}>WILSON CHEN</span>
-                    <span className={styles.mobileSubTitle}>UNIVERSITY OF SOUTHERN CALIFORNIA</span>
+                    <span className={styles.mobileSubTitle}>chenwils@usc.edu</span>
                     <a href='/#/aboutMe'><p>ABOUT ME</p></a>
                     <a href='/#/resume'><p>RESUME</p></a>
                     <div className={styles.buttonSection}>
